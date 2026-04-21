bc8858125bc54b3845ca0fedf1d4729b9d486ca4
RaihanAW <111202214017@mhs.dinus.ac.id>
2026-04-21 08:21:20 +0700

    Update Exercise.jsx

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -28,7 +28,7 @@ function Exercise() {
           User Cards
         </h1>
         <div className="grid md:grid-cols-3 lg:grid-cols-5 gap-6 max-h-fit">
-          {getUsers.map((post, index) => <UserCard key={index} {...post}/>)}
+          {postsData.map((post, index) => <UserCard key={index} {...post}/>)}
         </div>
       </div>
     </>
